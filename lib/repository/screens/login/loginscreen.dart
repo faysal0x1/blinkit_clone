876dbfb3b09972e950a24de35e354a07146966d7
faysal0x1 <faysal35-3119@diu.edu.bd>
2026-02-25 21:26:18 +0600

    Splash Screen Page

---
+++ lib/repository/screens/login/loginscreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatelessWidget {
+  const Loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
