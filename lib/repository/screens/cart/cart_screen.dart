cbd593b7b9a4211c76cfd1894ffa847fb2aab996
faysal0x1 <faysal35-3119@diu.edu.bd>
2026-02-26 11:47:46 +0600

    Cloninge complete

--- lib/repository/screens/cart/cart_screen.dart
+++ lib/repository/screens/cart/cart_screen.dart
@@ -1,17 +1,179 @@
+import 'package:blinkit_clone/repository/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
+  TextEditingController searchController = TextEditingController();
 
+  CartScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("Cart Screen")],
-        ),
-      ),
-    );
+        body: Column(
+          children: [
+            SizedBox(height: 40,),
+
+            Stack(
+              children: [
+                Container(
+                  height: 190,
+                  width: double.infinity,
+                  color: Color(0XFFF7CB45),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          UiHelper.CustomText(
+                              text: "Blinkit in",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontSize: 15,
+                              fontFamily: "bold"),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          UiHelper.CustomText(
+                              text: "16 minutes",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontSize: 20,
+                              fontFamily: "bold")
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          UiHelper.CustomText(
+                              text: "HOME ",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontSize: 14),
+                          UiHelper.CustomText(
+                              text: "- Sujal Dave, Ratanada, Jodhpur (Raj)",
+                              color: Color(0XFF000000),
+                              fontweight: FontWeight.bold,
+                              fontSize: 14)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 20,
+                  bottom: 100,
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 30,
+                    left: 20,
+                    child: UiHelper.CustomTextField(controller: searchController))
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            UiHelper.CustomImage(img: "cart.png"),
+            SizedBox(
+              height: 20,
+            ),
+            UiHelper.CustomText(
+                text: "Reordering will be easy",
+                color: Color(0XFF000000),
+                fontweight: FontWeight.bold,
+                fontSize: 16,
+                fontFamily: "bold"),
+            UiHelper.CustomText(
+              text: "Items you order will show up here so you can buy",
+              color: Color(0XFF000000),
+              fontweight: FontWeight.bold,
+              fontSize: 12,
+            ),
+            UiHelper.CustomText(
+              text: "them again easily.",
+              color: Color(0XFF000000),
+              fontweight: FontWeight.bold,
+              fontSize: 12,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                UiHelper.CustomText(
+                    text: "Bestsellers",
+                    color: Color(0XFF000000),
+                    fontweight: FontWeight.bold,
+                    fontSize: 16,
+                    fontFamily: "bold")
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Stack(
+                  children: [
+                    UiHelper.CustomImage(img: "milk.png"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 95, left: 65),
+                      child: UiHelper.CustomButton(() {}),
+                    ),
+
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Stack(
+                  children: [
+                    UiHelper.CustomImage(img: "potato.png"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 95, left: 65),
+                      child: UiHelper.CustomButton(() {}),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Stack(
+                  children: [
+                    UiHelper.CustomImage(img: "tomato.png"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 95, left: 65),
+                      child: UiHelper.CustomButton(() {}),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
